c7246e79a838f06cb6e028f10cc027105759a221
Marcos Gamazo <gamazoruiz@gmail.com>
2021-01-19 22:15:46 +0100

    post api working

--- actions/actions.py
+++ actions/actions.py
@@ -46,40 +46,39 @@ class action_pregunta_uno(Action):
 
     def run (self, dispatcher, tracker, domain):
         buttons = []
-        buttons.append({"title": 'Menos de 1 segundo' , "payload": "/menos_un_segundo{'respuesta1':'Menos de 1 segundo'}"})
-        buttons.append({"title": 'Entre 1 y 5 segundos' , "payload": "/entre_uno_cinco{'respuesta1':'Entre 1 y 5 segundos'}"})
-        buttons.append({"title": 'Mas de 9 segundos' , "payload": "/mas_de_nueve{'respuesta1':'Mas de 9 segundos'}"})
+        buttons.append({"title": 'Menos de 1 segundo' , "payload": "/pregunta_dos{'respuesta1':'Menos de 1 segundo'}"})
+        buttons.append({"title": 'Entre 1 y 5 segundos' , "payload": "/pregunta_dos{'respuesta1':'Entre 1 y 5 segundos'}"})
+        buttons.append({"title": 'Mas de 9 segundos' , "payload": "/pregunta_dos{'respuesta1':'Mas de 9 segundos'}"})
         dispatcher.utter_message(text="¿Cuánto dura el sonido?", buttons=buttons)
-        SlotSet('respuesta1',buttons)
         #FollowupAction(name='action_listen')        
-        return[FollowupAction(name='action_pregunta_dos')]#, FollowupAction(name='action_listen')]
+        return[SlotSet('respuesta1',buttons)]#, FollowupAction(name='action_listen')]
 
 class action_pregunta_dos(Action):
     def name(self):
         return 'action_pregunta_dos'
     def run (self, dispatcher, tracker, domain):
         buttons = []
-        buttons.append({"title": 'Si' , "payload": "/affirm{'respuesta2':'Si'}"})
-        buttons.append({"title": 'No' , "payload": "/deny{'respuesta2':'No'}"})
+        buttons.append({"title": 'Si' , "payload": "/pregunta_tres{'respuesta2':'Si'}"})
+        buttons.append({"title": 'No' , "payload": "/pregunta_tres{'respuesta2':'No'}"})
         dispatcher.utter_message(text='¿Se escucha al principio un sonido corto y luego un silencio o un sonido menos intenso?', buttons=buttons)
-        return[SlotSet('respuesta2',buttons),FollowupAction('action_listen')]
+        return[SlotSet('respuesta2',buttons)]
 
 class action_pregunta_tres(Action):
     def name(self):
         return 'action_pregunta_tres'
     def run (self, dispatcher, tracker, domain):
         buttons = []
-        buttons.append({"title": 'Si' , "payload": "/affirm{'respuesta3':'Si'}"})
-        buttons.append({"title": 'No' , "payload": "/deny{'respuesta3':'No'}"})
-        dispatcher.utter_button_message(text='¿Hay un sonido inicial con un tono distinto al resto?', buttons=buttons)
-        return[SlotSet('respuesta3',buttons),FollowupAction('utter_opciones')]
+        buttons.append({"title": 'Si' , "payload": "/post_api{'respuesta3':'Si'}"})
+        buttons.append({"title": 'No' , "payload": "/post_api{'respuesta3':'No'}"})
+        dispatcher.utter_message(text='¿Hay un sonido inicial con un tono distinto al resto?', buttons=buttons)
+        return[SlotSet('respuesta3',buttons),FollowupAction('post_api')]
 
 
 ################################################
 # Post a la API con las respuestas del usuario
 ################################################
 
-class ActionPostRespuestas(Action):
+class action_post_api(Action):
     def name(self):
         return 'action_post_api'
     
@@ -88,16 +87,18 @@ class ActionPostRespuestas(Action):
         url = 'http://127.0.0.1:3000/clasificaciones/'
         headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
         nombre = tracker.get_slot('nombre')
-        respuesta1 = tracker.get_slot('respuesta1')
-        respuesta2 = tracker.get_slot('respuesta2')
-        respuesta3 = tracker.get_slot('respuesta3')
+        respuesta1 = tracker.get_slot('respuesta1')[0]['title']
+        respuesta2 = tracker.get_slot('respuesta2')[0]['title']
+        respuesta3 = tracker.get_slot('respuesta3')[0]['title']
         eco = tracker.get_slot('eco')
+
         query = '{"_id":"' + str(eco) + '",'
 
-        if nombre is not None:
+        if nombre is not "amigo":
             query += '"idUsuario":"' + str(nombre) + '",'
     
         query += '"Respuesta1":"' + str(respuesta1) + '",' + '"Respuesta2":"' + str(respuesta2) + '",' + '"Respuesta3":"' + str(respuesta3) + '"}'
+        #print(query)
         r = requests.post(url, data= query, headers=headers)
         print(r.status_code)
         return
