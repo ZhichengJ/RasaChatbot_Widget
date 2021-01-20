f4821be099fa4dfebc2a9643f09c1f38ee14eadf
Marcos Gamazo <gamazoruiz@gmail.com>
2021-01-20 03:45:28 +0100

    bug fixes

--- actions/actions.py
+++ actions/actions.py
@@ -9,7 +9,9 @@
 
 # from typing import Any, Text, Dict, List
 #
-import os, random, json
+import os
+import random
+import json
 import time
 import requests
 from rasa_sdk import Action, Tracker
@@ -21,57 +23,78 @@ from rasa_sdk.events import FollowupAction
 # Devuelve un sonido para analizar
 ################################################
 
+
 class action_dar_sonido(Action):
     def name(self):
         return 'action_dar_sonido'
-    
-    def run (self, dispatcher, tracker, domain):
+
+    def run(self, dispatcher, tracker, domain):
         #Nos da el path absoluto de un sonido aleatorio del directorio sounds
         file = random.choice(os.listdir("sounds"))
-        path = os.path.join("/Users/marcosgamazo/PycharmProjects/chatbot-TFG/sounds",file)
+        path = os.path.join(
+            "/Users/marcosgamazo/PycharmProjects/chatbot-TFG/sounds", file)
         dispatcher.utter_message(path)
-        return [SlotSet("eco",os.path.splitext(file)[0])]
+        return [SlotSet("eco", os.path.splitext(file)[0])]
 
 class ActionContinuar(Action):
     def name(self):
         return 'action_continuar'
-    def run (self, dispatcher, tracker, domain):
-        #time.sleep(2)
-        dispatcher.utter_message("¿Quieres continuar?")
-        return[]
+
+    def run(self, dispatcher, tracker, domain):
+        buttons=[]
+        buttons.append({"title": 'Si',
+                        "payload": "/clasificar"})
+        buttons.append({"title": 'No',
+                        "payload": "/salir"})
+        dispatcher.utter_message("¿Quieres continuar?", buttons=buttons)
+        return[FollowupAction('')]
+
 
 class action_pregunta_uno(Action):
     def name(self):
         return 'action_pregunta_uno'
 
-    def run (self, dispatcher, tracker, domain):
+    def run(self, dispatcher, tracker, domain):
         buttons = []
-        buttons.append({"title": 'Menos de 1 segundo' , "payload": "/pregunta_dos{'respuesta1':'Menos de 1 segundo'}"})
-        buttons.append({"title": 'Entre 1 y 5 segundos' , "payload": "/pregunta_dos{'respuesta1':'Entre 1 y 5 segundos'}"})
-        buttons.append({"title": 'Mas de 9 segundos' , "payload": "/pregunta_dos{'respuesta1':'Mas de 9 segundos'}"})
-        dispatcher.utter_message(text="¿Cuánto dura el sonido?", buttons=buttons)
-        #FollowupAction(name='action_listen')        
-        return[SlotSet('respuesta1',buttons)]#, FollowupAction(name='action_listen')]
+        buttons.append({"title": 'Menos de 1 segundo',
+                        "payload": "/pregunta_dos{'respuesta1':'Menos de 1 segundo'}"})
+        buttons.append({"title": 'Entre 1 y 5 segundos',
+                        "payload": "/pregunta_dos{'respuesta1':'Entre 1 y 5 segundos'}"})
+        buttons.append({"title": 'Mas de 9 segundos',
+                        "payload": "/pregunta_dos{'respuesta1':'Mas de 9 segundos'}"})
+        dispatcher.utter_message(
+            text="¿Cuánto dura el sonido?", buttons=buttons)
+        return[SlotSet('respuesta1', buttons)]
+
 
 class action_pregunta_dos(Action):
     def name(self):
         return 'action_pregunta_dos'
-    def run (self, dispatcher, tracker, domain):
+
+    def run(self, dispatcher, tracker, domain):
         buttons = []
-        buttons.append({"title": 'Si' , "payload": "/pregunta_tres{'respuesta2':'Si'}"})
-        buttons.append({"title": 'No' , "payload": "/pregunta_tres{'respuesta2':'No'}"})
-        dispatcher.utter_message(text='¿Se escucha al principio un sonido corto y luego un silencio o un sonido menos intenso?', buttons=buttons)
-        return[SlotSet('respuesta2',buttons)]
+        buttons.append(
+            {"title": 'Si', "payload": "/pregunta_tres{'respuesta2':'Si'}"})
+        buttons.append(
+            {"title": 'No', "payload": "/pregunta_tres{'respuesta2':'No'}"})
+        dispatcher.utter_message(
+            text='¿Se escucha al principio un sonido corto y luego un silencio o un sonido menos intenso?', buttons=buttons)
+        return[SlotSet('respuesta2', buttons)]
+
 
 class action_pregunta_tres(Action):
     def name(self):
         return 'action_pregunta_tres'
-    def run (self, dispatcher, tracker, domain):
+
+    def run(self, dispatcher, tracker, domain):
         buttons = []
-        buttons.append({"title": 'Si' , "payload": "/post_api{'respuesta3':'Si'}"})
-        buttons.append({"title": 'No' , "payload": "/post_api{'respuesta3':'No'}"})
-        dispatcher.utter_message(text='¿Hay un sonido inicial con un tono distinto al resto?', buttons=buttons)
-        return[SlotSet('respuesta3',buttons),FollowupAction('post_api')]
+        buttons.append(
+            {"title": 'Si', "payload": "/post_api{'respuesta3':'Si'}"})
+        buttons.append(
+            {"title": 'No', "payload": "/post_api{'respuesta3':'No'}"})
+        dispatcher.utter_message(
+            text='¿Hay un sonido inicial con un tono distinto al resto?', buttons=buttons)
+        return[SlotSet('respuesta3', buttons), FollowupAction('post_api')]
 
 
 ################################################
@@ -81,7 +104,7 @@ class action_pregunta_tres(Action):
 class action_post_api(Action):
     def name(self):
         return 'action_post_api'
-    
+
     def run(self, dispatcher, tracker, domain):
         #url = 'http://138.100.100.143:3001/clasificaciones/'
         url = 'http://127.0.0.1:3000/clasificaciones/'
@@ -94,11 +117,12 @@ class action_post_api(Action):
 
         query = '{"_id":"' + str(eco) + '",'
 
-        if nombre is not "amigo":
+        if nombre is not None:
             query += '"idUsuario":"' + str(nombre) + '",'
-    
-        query += '"Respuesta1":"' + str(respuesta1) + '",' + '"Respuesta2":"' + str(respuesta2) + '",' + '"Respuesta3":"' + str(respuesta3) + '"}'
+
+        query += '"Respuesta1":"' + str(respuesta1) + '",' + '"Respuesta2":"' + str(
+            respuesta2) + '",' + '"Respuesta3":"' + str(respuesta3) + '"}'
         #print(query)
-        r = requests.post(url, data= query, headers=headers)
+        r = requests.post(url, data=query, headers=headers)
         print(r.status_code)
-        return
+        return [FollowupAction('action_continuar')]
